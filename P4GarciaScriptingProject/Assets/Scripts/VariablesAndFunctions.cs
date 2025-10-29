49753a437c4e069f33a1f85ed6f50cc61bc5c662
749759-Cloud <749759@student.dusd.net>
2025-10-29 11:03:11 -0700

    coding
    adding intagers

---
+++ P4GarciaScriptingProject/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    //create my integer variable
+   public  int myInt = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run my variable thru my function
+        myInt = MultipleByTwo(myInt);
+        Debug.Log(myInt);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //create the multibly by two function
+    int MultiplyBytwo(int number)
+    {
+        //create a local variable
+        int result;
+
+        //multiplying the num,ber by two
+        result = 
+    }
+
+}
+
+
+
+
+
